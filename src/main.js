569d7869c9ca11059295335c2314ce85e5dc5b19
gedons <gedoniani@gmail.com>
2023-09-18 11:56:37 +0100

    bug fixes

--- src/main.js
+++ src/main.js
@@ -5,18 +5,18 @@ import router from './router'
 import App from './App.vue'
 import 'vue-toast-notification/dist/theme-default.css'; 
 import VueToast from 'vue-toast-notification';
-import VueGoogleMaps from '@fawmi/vue-google-maps';
+// import VueGoogleMaps from '@fawmi/vue-google-maps';
 
 
 createApp(App)
     .use(store)   
     .use(VueToast)
-    .use(VueGoogleMaps, {
-        load: {
-          key: 'AIzaSyAhy2KmWLLpUUIgo3caZ16VtGsd60VFOWA',
-          libraries: 'places', 
-        },
-      })
+    // .use(VueGoogleMaps, {
+    //     load: {
+    //       key: 'AIzaSyAhy2KmWLLpUUIgo3caZ16VtGsd60VFOWA',
+    //       libraries: 'places', 
+    //     },
+    //   })
     .use(router) 
     .mount('#app')
 

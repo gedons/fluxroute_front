f1b0950d489857d0012c978c629bf2cf70c8ae59
gedons <gedoniani@gmail.com>
2023-10-26 15:52:33 +0100

    API endpoint updated

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,7 @@ import router from "./router";
 
 const axiosClient = axios.create({
 //   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
-  baseURL: `http://localhost:8000/api`
+  baseURL: `https://fluxroute.vercel.app/api`
 })
 
 axiosClient.interceptors.request.use(config => {

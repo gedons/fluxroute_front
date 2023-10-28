057fdc425d09e1491bcbd4b17d1bdbfc418f709b
gedons <gedoniani@gmail.com>
2023-10-28 08:52:16 +0100

    vite api base url updated

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ import router from "./router";
 
 
 const axiosClient = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}api`
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
   //baseURL: `https://fluxroute-api.vercel.app/api`
 })
 
